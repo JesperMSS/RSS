3ef1606d847423653bbc7bd3e6afc90c0ce4ee9b
Jonas-Hestreus <65901937+Jonas-Hestreus@users.noreply.github.com>
2021-10-21 12:42:45 +0200

    Just changed some stuff
    TEsting

--- Models/Program.cs
+++ Models/Program.cs
@@ -7,6 +7,7 @@ namespace Models
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing github");
         }
     }
 }
