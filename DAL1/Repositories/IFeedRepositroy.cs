0f79cb6ff4dd2fc12c30f6fd979a069543894489
Jonas-Hestreus <65901937+Jonas-Hestreus@users.noreply.github.com>
2021-10-25 15:30:31 +0200

    Fixat allt

--- DAL1/Repositories/IFeedRepositroy.cs
+++ DAL1/Repositories/IFeedRepositroy.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DAL.Repositories
 {
-    public interface EmptyInterface
+    interface IFeedRepositroy<T> : IRepository<T> where T : Feed
     {
+        Task<List<Episode>> getEpisodes(string url);
+        int GetIndexOfCategory(string name);
     }
+
 }
--- DAL1/Repositories/IRepository.cs
+++ DAL1/Repositories/IRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
 namespace DAL.Repositories
 {
-    public class EmptyClass
+    interface IRepository<T> where T : class
     {
-        public EmptyClass()
-        {
-        }
+        void Create(T media);
+        void Delete(int i);
+        void Update(int i, T newMedia);
+        List<T> GetAll();
+        void SaveChanges();
+        int GetIndexOfName(string name);
     }
 }
